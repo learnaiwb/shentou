35177f769b80132293d643e995fcf37deff31f5b
learnaiwb <975824857@qq.com>
2019-04-30 09:41:47 +0800

    Adding new modules

---
+++ modules/dirlister.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import os
+def run(**kwargs)
+    print('[*] In dirlister module')
+    files = os.listdir('.')
+    return str(files)
---
+++ modules/environment.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import os
+def run(**kwargs):
+    print('[*] In environment module')
+    return str(os.environ)
